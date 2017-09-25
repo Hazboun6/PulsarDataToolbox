ed4fc4a4749f4ef88b89993ed2f8503a49d80967
Jeffrey Hazboun <jeffrey.hazboun@gmail.com>
2017-09-25 08:18:05 -0700

    Test commit after PyPI setup.

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -20,7 +20,7 @@ except ImportError:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'hazboun6/pypsrfits'
+GITHUB_REPO = 'Hazboun6/pypsrfits'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
@@ -58,7 +58,8 @@ def fetch_public_key(repo):
     Travis API docs: http://docs.travis-ci.com/api/#repository-keys
     """
     keyurl = 'https://api.travis-ci.org/repos/{0}/key'.format(repo)
-    data = json.loads(urlopen(keyurl).read().decode())
+    print(keyurl)
+    data = json.loads(urlopen(keyurl).read().decode('utf-8'))
     if 'key' not in data:
         errmsg = "Could not find public key for repo: {}.\n".format(repo)
         errmsg += "Have you already added your GitHub repo to Travis?"
