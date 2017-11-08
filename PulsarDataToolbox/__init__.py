6b3cd7b13108ac9a5698d58fb3ef4d2e13557ce4
Jeffrey Hazboun <jeffrey.hazboun@gmail.com>
2017-11-08 10:56:39 -0500

    Added import of psrfits class to init.

--- PulsarDataToolbox/__init__.py
+++ PulsarDataToolbox/__init__.py
@@ -2,6 +2,8 @@
 
 """Top-level package for PulsarDataToolbox."""
 
+from .PulsarDataToolbox import psrfits
+
 __author__ = """Jeffrey S Hazboun"""
 __email__ = 'jeffrey.hazboun@gmail.com'
 __version__ = '0.1.0'
