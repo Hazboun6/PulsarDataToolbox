112ab65d822bda19030c50f19f87678d4e4ec9ad
Jeffrey Hazboun <jeffrey.hazboun@gmail.com>
2017-11-10 14:57:23 -0500

    Fix cli test usage.

--- pdat/cli.py
+++ pdat/cli.py
@@ -9,7 +9,7 @@ import click
 def main(args=None):
     """Console script for PulsarDataToolbox."""
     click.echo("Replace this message by putting your code into "
-               "PulsarDataToolbox.cli.main")
+               "pdat.cli.main")
     click.echo("See click documentation at http://click.pocoo.org/")
 
 
