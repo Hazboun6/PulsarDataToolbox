32d5e301b70bb507ec6464d06aa1ec254b3666e4
Jeffrey Hazboun <jeffreyhazboun@Jeffreys-iMac.local>
2017-12-05 08:14:32 -0800

    Changed version.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='pdat',
-    version='0.1.3',
+    version='0.1.4',
     description="Python package for dealing with PSRFITS and other pulsar data files.",
     long_description=readme + '\n\n' + history,
     author="Jeffrey S Hazboun",
